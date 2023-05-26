797735819b7f219d76f466a42d3f83335d1afc2c
mleiras <monicarafaelaleiras@gmail.com>
2023-05-26 15:39:24 +0100

    graphs assembly sequences

--- src/pydna/gg_graphs.py
+++ src/pydna/gg_graphs.py
@@ -20,13 +20,6 @@ def graph_assembly(list_seqs: list):
         graph
     '''
     G = nx.DiGraph()
-    # print(list_seqs)
-
-    # for index, elem in enumerate(layers):        
-    #     if(index<(len(layers)-1)):    
-    #         thiseLayer= elem        
-    #         nexteLayer= layers[index+1]
-
 
     for index, sequence in enumerate(list_seqs):
         lista = list_seqs.copy()
@@ -38,19 +31,44 @@ def graph_assembly(list_seqs: list):
                 node2 = list_seqs.index(seq)+1#,seq.figure().split('\n')[0][12:14])
                 G.add_node(node1, size = sequence.figure().split('\n')[0][12:14], dseq=sequence)
                 G.add_node(node2, size = seq.figure().split('\n')[0][12:14], dseq=seq)
-                G.add_edge(node1,node2)
-                # G.add_node((index,sequence.figure().split('\n')[0])) # em vez de figure tem de ser a sequencia apenas, algo do genero?
-                # G.add_node((list_seqs.index(seq),seq.figure().split('\n')[0]))
-                # G.add_edge(sequence.figure().split('\n')[0],seq.figure().split('\n')[0])
+                G.add_edge(node1,node2, w=new_seq)
             except:
                 pass
-
-    print(G)
     return G
 
 
 def find_all_paths(graph):
+    '''
+    find_all_paths is a function that returns a list of all the paths in a graph
+
+    Parameters
+    ----------
+    graph : _type_
+        a graph with multiple nodes and edges (it can be directed or not)
+
+    Returns
+    -------
+    all_paths : list
+        a list of all the possible paths in the graph
+    '''
     def find_paths(start, end, path=[]):
+        '''
+        find_paths  is a secondary function for recursive use in all the nodes of the graph
+
+        Parameters
+        ----------
+        start : _type_
+            node of a graph to start the path
+        end : _type_
+            node of a graph that ends the path
+        path : list, optional
+            list of the current path when searching, by default []
+
+        Returns
+        -------
+        paths : list
+            list of all the nodes included in the current path
+        '''
         path = path + [start]
         if start == end and len(path) > 1: #dif
             return [path]
@@ -75,22 +93,62 @@ def find_all_paths(graph):
         circular_paths = find_paths(start, start)
         all_paths.extend(circular_paths)
 
-    for path in all_paths:
-        print(path)
-
     return all_paths
 
 
+def find_paths_seqs(paths, grafo):
+    sequencias = {}
+    for path in paths:
+        # print(path)
+        soma = None
+        circular = False
+        if len(set(path)) != len(path):
+            circular = True
+        for i, seq in enumerate(list(set(path))):
+            
+            if i > len(list(set(path)))-1:
+                break
+            if soma is None:
+                soma = grafo.nodes[seq]['dseq']
+                # print('soma inicial',i, soma)
+            else:
+                # print()
+                # print('i',i,  grafo.nodes[seq])
+                # print('i', grafo.nodes[seq+1])
+                soma += grafo.nodes[seq]['dseq']
+        # print('SOMA', soma.figure())
+        # print(soma)
+        if circular:
+            soma = Dseqrecord(soma, circular=True)
+
+        sequencias[(tuple(path))] = soma
+
+    # print(sequencias)
+    return sequencias
+
+
 def to_graphviz(g):
+    '''
+    to_graphviz is a secondary function that prints code for using in the website GraphvizOnline for better visualization of the graph. Link: https://dreampuf.github.io/GraphvizOnline/ 
+
+    Parameters
+    ----------
+    g : _type_
+        graph (it can be a object of NetworkX graph)
+
+    Returns
+    -------
+    _type_
+        Code printed for copy+paste in the website GraphvizOnline
+    '''
     with io.StringIO() as F:
-        print(f'\n Code to GraphvizOnline: \n') #(https://dreampuf.github.io/GraphvizOnline/)')
         print("""digraph{
             node[shape = "circle", style = "filled"];
             """, file = F)
         for u in g:
             for v in g[u]:
                 if g[u][v] is not {}:
-                    print(f'{u} -> {v}[label = ""];', file = F)
+                    print(f'{u} -> {v}[label = "{g[u][v]}"];', file = F)
                     # print(f'{u} -> {v}[label = ""];', file = F) # backup
                     # print(f"{u} -> {v};", file = F)
                 else:
@@ -102,8 +160,6 @@ def to_graphviz(g):
 
 
 if __name__ == '__main__':
-
-    
 
     a = Dseqrecord("CTTAAGatgccctaaccccGAATTC")
     b = Dseqrecord("GAATTCatgccctaaccccGAATTC")
@@ -118,6 +174,10 @@ if __name__ == '__main__':
     # d = Dseqrecord("CTTAAGatgccctttttttGAATTC")
     # s4,d2 = d.cut(EcoRI)
 
+    # só funciona com Dseq (em vez de Dseqrecord):
+    # print('5', s1.five_prime_end())
+    # print('3', s1.three_prime_end())
+    # print()
 
     # print(s1.figure())
     # print()
@@ -126,8 +186,8 @@ if __name__ == '__main__':
     # print(s3.figure())
     # print()
 
-    d = s1+s2+s3
-    e = s1+s3
+    # d = s1+s2+s3
+    # e = s1+s3
     # print(d.figure())
     # print(e.figure())
     # print(repr(s1))
@@ -139,31 +199,28 @@ if __name__ == '__main__':
     
     lista_seqs = [s1,s2,s3]
 
-
-    print()
-    print('GRAPH: \n')
-
     grafo = graph_assembly(lista_seqs)
+    # print(grafo)
+    # print('Nodes: ', grafo.nodes)
+    # print('Edges: ', grafo.edges)
 
-    print(to_graphviz(grafo))
-    find_all_paths(grafo)
-    print()
+    # print()
+
+    # print()
+    # print(to_graphviz(grafo))
+
+    # print('All the paths on the graph: ')
+    paths = find_all_paths(grafo)
+    # print()
+    # for path in find_all_paths(grafo):
+    #     print(path)
+    # print()
+
+    dic_paths = find_paths_seqs(paths, grafo)
+    for i,x in dic_paths.items():
+        print(i)
+        print(x.figure())
+
+    # print(unique_paths(paths, grafo))
     # print(grafo.nodes[0]['dseq']) # forma de aceder à informação toda da sequencia em questão
-
-
-
-
-    # só funciona com Dseq (em vez de Dseqrecord)
-
-    # print('5', s1.five_prime_end())
-    # print('3', s1.three_prime_end())
-    # print()
-
-    # print('5', s2.five_prime_end())
-    # print('3', s2.three_prime_end())
-    # print()
-
-    # print('5', s3.five_prime_end())
-    # print('3', s3.three_prime_end())
-
 
