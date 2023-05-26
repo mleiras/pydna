36ae78a09f76174f3a459e1c3bca7bc454a84e7e
mleiras <monicarafaelaleiras@gmail.com>
2023-05-26 11:30:19 +0100

    graphs gg with paths

--- src/pydna/gg_graphs.py
+++ src/pydna/gg_graphs.py
@@ -19,7 +19,7 @@ def graph_assembly(list_seqs: list):
     _type_
         graph
     '''
-    G = nx.Graph()
+    G = nx.DiGraph()
     # print(list_seqs)
 
     # for index, elem in enumerate(layers):        
@@ -34,28 +34,56 @@ def graph_assembly(list_seqs: list):
         for i, seq in enumerate(lista):
             try: 
                 new_seq = sequence + seq
-                # print(index, i, '1try')
-                G.add_node(sequence.figure()) # em vez de figure tem de ser a sequencia apenas, algo do genero?
-                G.add_node(seq.figure())
-                G.add_edge(sequence.figure(),seq.figure())
-                # print('new_seq', new_seq.figure())
-                # print()
+                node1 = index+1 #,sequence.figure().split('\n')[0][12:14]) # index da sequencia (na lista original) + tamanho da sequencia para referencia
+                node2 = list_seqs.index(seq)+1#,seq.figure().split('\n')[0][12:14])
+                G.add_node(node1, size = sequence.figure().split('\n')[0][12:14], dseq=sequence)
+                G.add_node(node2, size = seq.figure().split('\n')[0][12:14], dseq=seq)
+                G.add_edge(node1,node2)
+                # G.add_node((index,sequence.figure().split('\n')[0])) # em vez de figure tem de ser a sequencia apenas, algo do genero?
+                # G.add_node((list_seqs.index(seq),seq.figure().split('\n')[0]))
+                # G.add_edge(sequence.figure().split('\n')[0],seq.figure().split('\n')[0])
             except:
                 pass
 
-
-    
-        # if seq + list_seqs[index+1]:
-        #     new_seq = seq + list_seqs[index+1]
-        #     print(index, new_seq.figure())
-
     print(G)
-
     return G
+
+
+def find_all_paths(graph):
+    def find_paths(start, end, path=[]):
+        path = path + [start]
+        if start == end and len(path) > 1: #dif
+            return [path]
+        if start not in graph:
+            return []
+        paths = []
+        for node in graph[start]:
+            if node not in path or (node == end): #dif
+                newpaths = find_paths(node, end, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+
+    all_paths = []
+    for start in graph:
+        for end in graph:
+            if start != end:
+                paths = find_paths(start, end)
+                all_paths.extend(paths)
+
+        # Add circular paths
+        circular_paths = find_paths(start, start)
+        all_paths.extend(circular_paths)
+
+    for path in all_paths:
+        print(path)
+
+    return all_paths
 
 
 def to_graphviz(g):
     with io.StringIO() as F:
+        print(f'\n Code to GraphvizOnline: \n') #(https://dreampuf.github.io/GraphvizOnline/)')
         print("""digraph{
             node[shape = "circle", style = "filled"];
             """, file = F)
@@ -63,6 +91,7 @@ def to_graphviz(g):
             for v in g[u]:
                 if g[u][v] is not {}:
                     print(f'{u} -> {v}[label = ""];', file = F)
+                    # print(f'{u} -> {v}[label = ""];', file = F) # backup
                     # print(f"{u} -> {v};", file = F)
                 else:
                     print(f'{u} -> {v}[label = "{g[u][v]}"];', file = F)
@@ -86,6 +115,9 @@ if __name__ == '__main__':
     s1,a2  = a.cut(EcoRI)
     b1,s2,b3 = b.cut(EcoRI)
     c1,s3 = c.cut(EcoRI)
+    # d = Dseqrecord("CTTAAGatgccctttttttGAATTC")
+    # s4,d2 = d.cut(EcoRI)
+
 
     # print(s1.figure())
     # print()
@@ -107,12 +139,18 @@ if __name__ == '__main__':
     
     lista_seqs = [s1,s2,s3]
 
+
     print()
     print('GRAPH: \n')
 
     grafo = graph_assembly(lista_seqs)
 
     print(to_graphviz(grafo))
+    find_all_paths(grafo)
+    print()
+    # print(grafo.nodes[0]['dseq']) # forma de aceder à informação toda da sequencia em questão
+
+
 
 
     # só funciona com Dseq (em vez de Dseqrecord)
